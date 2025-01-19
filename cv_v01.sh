d21ff16c5e1112bd72931d088532a173e739d738
agilbagus16 <agilbagusbayuaji16@gmail.com>
2025-01-19 16:33:23 +0700

    Add files via upload

---
+++ cv_v01.sh
@@ -0,0 +1,75 @@
+#!/system/bin/sh
+
+
+clear
+sleep 1
+clear
+figlet mulai
+figlet V0.0.1
+echo
+echo "====================="
+echo "PROGRAM INPUT CV"
+echo "====================="
+echo
+echo "Siapa nama anda ?"
+echo
+read nama
+echo
+echo "Berapa usia anda"
+echo
+read usia
+echo 
+echo "Tempat dan Tanggal lahir"
+echo
+read ttd
+echo
+sleep 2
+echo "=============================================="
+echo "Halo nama saya $nama, Usia saya $usia, Saya lahir di $ttd"
+echo "=============================================="
+echo
+echo "Tekan enter untuk melanjutkan!!!"
+read enter
+sleep 1
+clear
+echo "APLIKASI SEGERA DITUTUP DALAM 10 DETIK!!!"
+sleep 4
+clear
+echo "##########"
+sleep 0.2
+clear
+echo "#########"
+sleep 0.2
+clear
+echo "########"
+sleep 0.2
+clear
+echo "#######"
+sleep 0.2
+clear
+echo "######"
+sleep 0.2
+clear
+echo "#####"
+sleep 0.2
+clear
+echo "####"
+sleep 0.2
+clear
+echo "###"
+sleep 0.2
+clear
+echo "##"
+sleep 0.2
+clear
+echo "#"
+sleep 0.2
+clear
+figlet terimakasih
+echo "####################"
+echo "Author Agilbgsb.a"
+echo "####################"
+echo "V.0.0.1"
+now=$(date +"%F/%T")
+echo "$: TANGGAL/WAKTU : $now :$"
+pwd
